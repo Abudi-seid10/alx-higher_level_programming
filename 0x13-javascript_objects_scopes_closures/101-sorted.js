91be790e8b4c8110d87a8b979244482c51201551
Abudi Seid (Wolfsighter) <54111629+Abudi-seid10@users.noreply.github.com>
2022-10-05 01:41:42 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
-}
-console.log(newDict);
+const arrayInit = require('./100-data').list;
+
+const newArray = arrayInit.map(function (element, index) {
+  return (element * index);
+});
+
+console.log(arrayInit);
+console.log(newArray);
