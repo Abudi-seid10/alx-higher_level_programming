88242fdb1f35e548220c7d088051a32f02777496
Abudi Seid (Wolfsighter) <54111629+Abudi-seid10@users.noreply.github.com>
2022-10-05 01:49:02 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
-const arrayInit = require('./100-data').list;
+const dict = require('./101-data').dict;
 
-const newArray = arrayInit.map(function (element, index) {
-  return (element * index);
-});
-
-console.log(arrayInit);
-console.log(newArray);
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
+  }
+  newDict[valsUniq[j]] = list;
+}
+console.log(newDict);
